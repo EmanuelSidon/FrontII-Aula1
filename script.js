bdc20c20afaf1fe569ca15d8ba9f79e86543dde5
Emanuel Sidon <emanuel-ferrera@hotmail.com>
2023-02-14 19:12:10 -0300

    Aula-1/Exercicio1.1

---
+++ script.js
@@ -0,0 +1,11 @@
+let userName;
+let userAge;
+let job;
+
+userName = window.prompt("Bem vindo, por favor informe seu nome: ");
+
+userAge = window.prompt("Agora me informe sua idade: ");
+
+job = window.prompt("E por ultimo, me diga sua profissão: ");
+
+const welcomeMessage = alert(`Muito bem vindo ${userName}, sua idade é de ${userAge} e gostei de saber que trabalha como ${job}`);
